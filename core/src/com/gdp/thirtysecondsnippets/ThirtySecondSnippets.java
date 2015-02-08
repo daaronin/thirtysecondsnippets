72488eafbe036125046ed57033383c2de2801ec4
Daniel Ford (PC) <daaronin@yahoo.com>
2015-02-08 14:12:12 -0500

    Setup touch/mouse controls - player can only move on left 3rd of screen Added scrolling background Made the game Beautiful

--- core/src/com/gdp/thirtysecondsnippets/ThirtySecondSnippets.java
+++ core/src/com/gdp/thirtysecondsnippets/ThirtySecondSnippets.java
@@ -1,84 +1,201 @@
 package com.gdp.thirtysecondsnippets;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Input.Buttons;
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.TimeUtils;
 
-public class ThirtySecondSnippets extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-        Sprite sprite;
-	float posX, posY;
-        
-	@Override
-	public void create () {
-                float w = Gdx.graphics.getWidth();
-                float h = Gdx.graphics.getHeight();
-		
-                batch = new SpriteBatch();
-		img = new Texture("yarn.png");
-                sprite = new Sprite(img);
-                
-                posX = w/2 - sprite.getWidth()/2;
-                posY = h/2 - sprite.getHeight()/2;
-                sprite.setPosition(posX,posY);
-                
-                //Gdx.input.setInputProcessor(this);
-                
-	}
+public class ThirtySecondSnippets implements ApplicationListener, InputProcessor {
+    SpriteBatch batch;
+    Texture img, background;
+    Sprite sprite;
+    float posX, posY;
+    float width, height;
+    float bgx;
+    long lastTimeBg;
+    
+    @Override
+    public void create () {
+            width = Gdx.graphics.getWidth();
+            height = Gdx.graphics.getHeight();
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 1, 1, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		
-                 
-                if(Gdx.input.isKeyPressed(Input.Keys.LEFT)){
-                    if(Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT))
-                        sprite.translateX(-1f);
-                    else
-                        sprite.translateX(-10.0f);
-                }
-                if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)){
-                    if(Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT))
-                        sprite.translateX(1f);
-                    else
-                        sprite.translateX(10.0f);
-                }
-                if(Gdx.input.isKeyPressed(Input.Keys.UP)){
-                    if(Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT))
-                        sprite.translateY(1f);
-                    else
-                        sprite.translateY(10.0f);
-                }
-                if(Gdx.input.isKeyPressed(Input.Keys.DOWN)){
-                    if(Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT))
-                        sprite.translateY(-1f);
-                    else
-                        sprite.translateY(-10.0f);
-                }
-                
-                batch.begin();
-		sprite.draw(batch);
-		batch.end();
-	}
+            //width = 800;
+            //height = 480;
+            bgx = 800;
+            
+            batch = new SpriteBatch();
+            img = new Texture("smallcat.png");
+            background = new Texture("backgroundstars.jpg");
+            sprite = new Sprite(img);
+
+            posX = width/8 - sprite.getWidth()/2;
+            posY = height/2 - sprite.getHeight()/2;
+            sprite.setPosition(posX,posY);
+            
+            // set lastTimeBg to current time
+            lastTimeBg = TimeUtils.nanoTime();
+
+            Gdx.input.setInputProcessor(this);
+
+    }
+
+    @Override
+    public void render () {
+            Gdx.gl.glClearColor(1, 1, 1, 1);
+            Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+
+            if(Gdx.input.isKeyPressed(Input.Keys.LEFT)){
+                if(Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT))
+                    sprite.translateX(-1f);
+                else
+                    sprite.translateX(-10.0f);
+            }
+            if(Gdx.input.isKeyPressed(Input.Keys.RIGHT) && checkPlayerX((int)posX)){
+                if(Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT))
+                    sprite.translateX(1f);
+                else
+                    sprite.translateX(10.0f);
+            }
+            if(Gdx.input.isKeyPressed(Input.Keys.UP)){
+                if(Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT))
+                    sprite.translateY(1f);
+                else
+                    sprite.translateY(10.0f);
+            }
+            if(Gdx.input.isKeyPressed(Input.Keys.DOWN)){
+                if(Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT))
+                    sprite.translateY(-1f);
+                else
+                    sprite.translateY(-10.0f);
+            }
+            
+            // move the separator each 1s
+            if(TimeUtils.nanoTime() - lastTimeBg > 100000000){
+                    // move the separator 50px
+                    bgx -= 50;
+                    // set the current time to lastTimeBg
+                    lastTimeBg = TimeUtils.nanoTime();
+            }
+
+            // if the seprator reaches the screen edge, move it back to the first position
+            if(bgx == 0){
+                    bgx = 800;
+            }
+
+            batch.begin();
+
+            batch.draw(background, bgx - 800, 0);
+            batch.draw(background, bgx, 0);
+            
+            sprite.draw(batch);
+            batch.end();
+    }
         
-       /* @Override
+        @Override
         public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-            if(button == Buttons.LEFT){
-                posX = screenX - sprite.getWidth()/2;
+            if(button == Buttons.LEFT && checkPlayerX(screenX)){
+                posX = screenX - sprite.getWidth();
                 posY = Gdx.graphics.getHeight() - screenY - sprite.getHeight()/2;
+                sprite.setPosition(posX,posY);
             }
             if(button == Buttons.RIGHT){
                 posX = Gdx.graphics.getWidth()/2 - sprite.getWidth()/2;
                 posY = Gdx.graphics.getHeight()/2 - sprite.getHeight()/2;
+                sprite.setPosition(posX,posY);
             }
             return false;
         }
-        */
+        
+
+    @Override
+    public void resize(int width, int height) {
+        width = Gdx.graphics.getWidth();
+        height = Gdx.graphics.getHeight();
+        posX = width/8 - sprite.getWidth()/2;
+        posY = height/2 - sprite.getHeight()/2;
+        sprite.setPosition(posX,posY);
+    }
+
+    @Override
+    public void pause() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void resume() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void dispose() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean keyDown(int keycode) {
+        float moveAmount = 1.0f;
+        if(Gdx.input.isKeyPressed(Keys.CONTROL_LEFT))
+            moveAmount = 10.0f;
+        if(keycode == Keys.LEFT)
+            posX-=moveAmount;
+        if(keycode == Keys.RIGHT && checkPlayerX((int)posX))
+            posX+=moveAmount;
+        return true;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        return true;
+    }
+
+    @Override
+    public boolean keyTyped(char character) {
+        return true;
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return true;
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        if(pointer == Buttons.LEFT){
+            if (checkPlayerX(screenX))
+                posX = screenX - sprite.getWidth();
+ 
+            posY = Gdx.graphics.getHeight() - screenY - sprite.getHeight()/2;
+            sprite.setPosition(posX,posY);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return true;
+    }
+
+    @Override
+    public boolean scrolled(int amount) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    public boolean checkPlayerX(int screenX){
+        float mouseX = screenX - sprite.getWidth();
+        if (mouseX <= Gdx.graphics.getWidth()/6){
+            return true;
+        }
+        return false;
+    }
 }
