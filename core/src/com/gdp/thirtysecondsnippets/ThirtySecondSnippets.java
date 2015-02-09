b7e49366c10475d1b71d0bb7def742f1c3e369cf
Daniel Ford (PC) <daaronin@yahoo.com>
2015-02-08 20:59:42 -0500

    Fixed Resize For Realzies

--- core/src/com/gdp/thirtysecondsnippets/ThirtySecondSnippets.java
+++ core/src/com/gdp/thirtysecondsnippets/ThirtySecondSnippets.java
@@ -123,12 +123,12 @@ public class ThirtySecondSnippets implements ApplicationListener, InputProcessor
 
     @Override
     public void pause() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public void resume() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
