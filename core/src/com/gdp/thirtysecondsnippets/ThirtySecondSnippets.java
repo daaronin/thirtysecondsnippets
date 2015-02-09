1784ea645889a3b529589e8ad62ba883d02aee8d
George <georgemcdaid@gmail.com>
2015-02-09 01:56:49 -0500

    Let's get musical

--- core/src/com/gdp/thirtysecondsnippets/ThirtySecondSnippets.java
+++ core/src/com/gdp/thirtysecondsnippets/ThirtySecondSnippets.java
@@ -7,13 +7,25 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Input.Buttons;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.TimeUtils;
+import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class ThirtySecondSnippets implements ApplicationListener, InputProcessor {
+
     SpriteBatch batch;
     Texture threadlet, background, scissors;
     Sprite player_sprite, scissors_sprite;
@@ -22,113 +34,139 @@ public class ThirtySecondSnippets implements ApplicationListener, InputProcessor
     float width, height;
     float bgx;
     long lastTimeBg;
-    
+
     @Override
-    public void create () {
-            width = Gdx.graphics.getWidth();
-            height = Gdx.graphics.getHeight();
+    public void create() {
+        try {
+            String filename = "music.mp3";
+            InputStream is = new URL("http://d318706lgtcm8e.cloudfront.net/mp3-preview/f454c8224828e21fa146af84916fd22cb89cedc6").openStream();
+            BufferedInputStream stream = new BufferedInputStream(is);
+            ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+            FileHandle handle = Gdx.files.external(filename);
+            if (handle.exists()) {
+                handle.delete();
+            }
+            int current = 0;
+            while ((current = stream.read()) != -1) {
+                bytes.write(current);
+            }
+            FileOutputStream fos = new FileOutputStream(handle.file());
+            bytes.writeTo(fos);
+
+            Music m = Gdx.audio.newMusic(handle);
+            m.play();
+            System.out.println(bytes.size());
+        } catch (MalformedURLException ex) {
+            Logger.getLogger(ThirtySecondSnippets.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(ThirtySecondSnippets.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        width = Gdx.graphics.getWidth();
+        height = Gdx.graphics.getHeight();
 
             //width = 800;
-            //height = 480;
-            bgx = 800;
-            
-            batch = new SpriteBatch();
-            threadlet = new Texture("smallcat.png");
-            background = new Texture("backgroundstars.jpg");
-            scissors = new Texture("scissors.png");
-            player_sprite = new Sprite(threadlet);
-            scissors_sprite = new Sprite(scissors);
-            
-            posX = width/8 - player_sprite.getWidth()/2;
-            posY = height/2 - player_sprite.getHeight()/2;
-            scissorsX = width * .65f - player_sprite.getWidth()/2;
-            scissorsY = height * .75f - player_sprite.getHeight()/2;
-            player_sprite.setPosition(posX,posY);
-            scissors_sprite.setPosition(scissorsX,scissorsY);
-            // set lastTimeBg to current time
-            lastTimeBg = TimeUtils.nanoTime();
+        //height = 480;
+        bgx = 800;
 
-            Gdx.input.setInputProcessor(this);
+        batch = new SpriteBatch();
+        threadlet = new Texture("smallcat.png");
+        background = new Texture("backgroundstars.jpg");
+        scissors = new Texture("scissors.png");
+        player_sprite = new Sprite(threadlet);
+        scissors_sprite = new Sprite(scissors);
+
+        posX = width / 8 - player_sprite.getWidth() / 2;
+        posY = height / 2 - player_sprite.getHeight() / 2;
+        scissorsX = width * .65f - player_sprite.getWidth() / 2;
+        scissorsY = height * .75f - player_sprite.getHeight() / 2;
+        player_sprite.setPosition(posX, posY);
+        scissors_sprite.setPosition(scissorsX, scissorsY);
+        // set lastTimeBg to current time
+        lastTimeBg = TimeUtils.nanoTime();
+
+        Gdx.input.setInputProcessor(this);
 
     }
 
     @Override
-    public void render () {
-            Gdx.gl.glClearColor(1, 1, 1, 1);
-            Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+    public void render() {
+        Gdx.gl.glClearColor(1, 1, 1, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-
-            if(Gdx.input.isKeyPressed(Input.Keys.LEFT)){
-                if(Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT))
-                    player_sprite.translateX(-1f);
-                else
-                    player_sprite.translateX(-10.0f);
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+            if (Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT)) {
+                player_sprite.translateX(-1f);
+            } else {
+                player_sprite.translateX(-10.0f);
             }
-            if(Gdx.input.isKeyPressed(Input.Keys.RIGHT) && checkPlayerX((int)posX)){
-                if(Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT))
-                    player_sprite.translateX(1f);
-                else
-                    player_sprite.translateX(10.0f);
-            }
-            if(Gdx.input.isKeyPressed(Input.Keys.UP)){
-                if(Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT))
-                    player_sprite.translateY(1f);
-                else
-                    player_sprite.translateY(10.0f);
-            }
-            if(Gdx.input.isKeyPressed(Input.Keys.DOWN)){
-                if(Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT))
-                    player_sprite.translateY(-1f);
-                else
-                    player_sprite.translateY(-10.0f);
-            }
-            
-            // move the separator each 1s
-            if(TimeUtils.nanoTime() - lastTimeBg > 100000000){
-                    // move the separator 50px
-                    bgx -= 50;
-                    scissors_sprite.translateX(-10f);
-                    // set the current time to lastTimeBg
-                    lastTimeBg = TimeUtils.nanoTime();
-            }
-
-            // if the seprator reaches the screen edge, move it back to the first position
-            if(bgx == 0){
-                    bgx = 800;
-            }
-            if(scissors_sprite.getX() <= -220){
-                    scissors_sprite.setX(650);
-            }
-
-            batch.begin();
-
-            batch.draw(background, bgx - 800, 0);
-            batch.draw(background, bgx, 0);
-            
-            player_sprite.draw(batch);
-            scissors_sprite.draw(batch);
-            batch.end();
-    }
-        
-        @Override
-        public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-            if(button == Buttons.LEFT && checkPlayerX(screenX)){
-                posX = screenX - player_sprite.getWidth();
-                posY = Gdx.graphics.getHeight() - screenY - player_sprite.getHeight()/2;
-                player_sprite.setPosition(posX,posY);
-            }
-            if(button == Buttons.RIGHT){
-                posX = Gdx.graphics.getWidth()/2 - player_sprite.getWidth()/2;
-                posY = Gdx.graphics.getHeight()/2 - player_sprite.getHeight()/2;
-                player_sprite.setPosition(posX,posY);
-            }
-            return false;
         }
-        
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT) && checkPlayerX((int) posX)) {
+            if (Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT)) {
+                player_sprite.translateX(1f);
+            } else {
+                player_sprite.translateX(10.0f);
+            }
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
+            if (Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT)) {
+                player_sprite.translateY(1f);
+            } else {
+                player_sprite.translateY(10.0f);
+            }
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+            if (Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT)) {
+                player_sprite.translateY(-1f);
+            } else {
+                player_sprite.translateY(-10.0f);
+            }
+        }
+
+        // move the separator each 1s
+        if (TimeUtils.nanoTime() - lastTimeBg > 100000000) {
+            // move the separator 50px
+            bgx -= 50;
+            scissors_sprite.translateX(-10f);
+            // set the current time to lastTimeBg
+            lastTimeBg = TimeUtils.nanoTime();
+        }
+
+        // if the seprator reaches the screen edge, move it back to the first position
+        if (bgx == 0) {
+            bgx = 800;
+        }
+        if (scissors_sprite.getX() <= -220) {
+            scissors_sprite.setX(650);
+        }
+
+        batch.begin();
+
+        batch.draw(background, bgx - 800, 0);
+        batch.draw(background, bgx, 0);
+
+        player_sprite.draw(batch);
+        scissors_sprite.draw(batch);
+        batch.end();
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        if (button == Buttons.LEFT && checkPlayerX(screenX)) {
+            posX = screenX - player_sprite.getWidth();
+            posY = Gdx.graphics.getHeight() - screenY - player_sprite.getHeight() / 2;
+            player_sprite.setPosition(posX, posY);
+        }
+        if (button == Buttons.RIGHT) {
+            posX = Gdx.graphics.getWidth() / 2 - player_sprite.getWidth() / 2;
+            posY = Gdx.graphics.getHeight() / 2 - player_sprite.getHeight() / 2;
+            player_sprite.setPosition(posX, posY);
+        }
+        return false;
+    }
 
     @Override
     public void resize(int width, int height) {
-        
+
     }
 
     @Override
@@ -149,12 +187,15 @@ public class ThirtySecondSnippets implements ApplicationListener, InputProcessor
     @Override
     public boolean keyDown(int keycode) {
         float moveAmount = 1.0f;
-        if(Gdx.input.isKeyPressed(Keys.CONTROL_LEFT))
+        if (Gdx.input.isKeyPressed(Keys.CONTROL_LEFT)) {
             moveAmount = 10.0f;
-        if(keycode == Keys.LEFT)
-            posX-=moveAmount;
-        if(keycode == Keys.RIGHT && checkPlayerX((int)posX))
-            posX+=moveAmount;
+        }
+        if (keycode == Keys.LEFT) {
+            posX -= moveAmount;
+        }
+        if (keycode == Keys.RIGHT && checkPlayerX((int) posX)) {
+            posX += moveAmount;
+        }
         return true;
     }
 
@@ -176,12 +217,13 @@ public class ThirtySecondSnippets implements ApplicationListener, InputProcessor
 
     @Override
     public boolean touchDragged(int screenX, int screenY, int pointer) {
-        if(pointer == Buttons.LEFT){
-            if (checkPlayerX(screenX))
+        if (pointer == Buttons.LEFT) {
+            if (checkPlayerX(screenX)) {
                 posX = screenX - player_sprite.getWidth();
- 
-            posY = Gdx.graphics.getHeight() - screenY - player_sprite.getHeight()/2;
-            player_sprite.setPosition(posX,posY);
+            }
+
+            posY = Gdx.graphics.getHeight() - screenY - player_sprite.getHeight() / 2;
+            player_sprite.setPosition(posX, posY);
         }
         return true;
     }
@@ -196,10 +238,10 @@ public class ThirtySecondSnippets implements ApplicationListener, InputProcessor
     public boolean scrolled(int amount) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
-    public boolean checkPlayerX(int screenX){
+
+    public boolean checkPlayerX(int screenX) {
         float mouseX = screenX - player_sprite.getWidth();
-        if (mouseX <= Gdx.graphics.getWidth()/6){
+        if (mouseX <= Gdx.graphics.getWidth() / 6) {
             return true;
         }
         return false;
