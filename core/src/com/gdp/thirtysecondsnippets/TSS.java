bc8ece63fe6d3251694e48da8644856b90b0b4fa
George <georgemcdaid@gmail.com>
2015-02-20 00:58:02 -0500

    Hey I just coded you and this is crazy, but display maybe

---
+++ core/src/com/gdp/thirtysecondsnippets/TSS.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.gdp.thirtysecondsnippets;
+
+import com.badlogic.gdx.Game;
+
+/**
+ *
+ * @author George McDaid
+ */
+public class TSS extends Game{
+    public static final String TITLE="Game Project"; 
+    public static final int WIDTH=900,HEIGHT=400; // used later to set window size
+    
+    @Override
+    public void create() {
+            setScreen(new GenreMenu());
+    }
+
+}
--- desktop/src/com/gdp/thirtysecondsnippets/desktop/DesktopLauncher.java
+++ desktop/src/com/gdp/thirtysecondsnippets/desktop/DesktopLauncher.java
@@ -2,11 +2,15 @@ package com.gdp.thirtysecondsnippets.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.gdp.thirtysecondsnippets.TSS;
 import com.gdp.thirtysecondsnippets.ThirtySecondSnippets;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new ThirtySecondSnippets(), config);
+		config.width=TSS.WIDTH; // sets window width
+                config.height=TSS.HEIGHT;  // sets window height
+                new LwjglApplication(new TSS(), config);
+
 	}
 }
