b054007b52511ff2f8a42d054ba09f0d4c506b03
George <georgemcdaid@gmail.com>
2015-02-21 03:46:18 -0500

    These buttons put me to sleep Now I'm callin them Nyquil

--- core/src/com/gdp/thirtysecondsnippets/GenreMenu.java
+++ core/src/com/gdp/thirtysecondsnippets/GenreMenu.java
@@ -14,11 +14,13 @@ import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import java.util.ArrayList;
 
 /**
  *
@@ -32,36 +34,44 @@ public class GenreMenu implements Screen{
     private Skin skin = new Skin(Gdx.files.internal("skin.json"),
         new TextureAtlas(Gdx.files.internal("buttons.pack")));
 
-    private TextButton buttonPlay = new TextButton("Slice dice and julienne fries", skin),
-        buttonExit = new TextButton("Bink", skin);
-
-
+    
+    
     @Override
     public void show() {
-        buttonPlay.addListener(new ChangeListener(){
-            @Override
-            public void changed(ChangeListener.ChangeEvent event, Actor actor) {
-                //Same way we moved here from the Splash Screen
-                //We set it to new Splash because we got no other screens
-                //otherwise you put the screen there where you want to go
-                System.out.println("Play");
+        MusicDB db = new MusicDB();
+        ArrayList<Genre> genres = db.getGenres();
+        
+        for(int i = 0;i < genres.size();i++){
+            if(i == ((genres.size() + genres.size()%2)/2)){
+                table.row();
             }
-        });
-    buttonExit.addListener(new ChangeListener(){
-        @Override
-            public void changed(ChangeListener.ChangeEvent event, Actor actor) {
-                Gdx.app.exit();
-            // or System.exit(0);
-            }
-    });
-    
-        //The elements are displayed in the order you add them.
-        //The first appear on top, the last at the bottom.
-        table.add(buttonPlay).size(350,120).padBottom(20).row();
-        table.add(buttonExit).size(350,120).padBottom(20).row();
+            
+            TextButton button = new TextButton(genres.get(i).getName(), skin);
+            
+            button.addListener(new ChangeListener(){
+                @Override
+                public void changed(ChangeListener.ChangeEvent event, Actor actor) {
+                    //Same way we moved here from the Splash Screen
+                    //We set it to new Splash because we got no other screens
+                    //otherwise you put the screen there where you want to go
+                    System.out.println("Play");
+                }
+            });
 
-        table.setFillParent(true);
-        stage.addActor(table);
+            //The elements are displayed in the order you add them.
+            //The first appear on top, the last at the bottom.
+            table.add(button).size(350,120).padBottom(10).padTop(10).padRight(10).padLeft(10);
+            
+            
+        }
+        
+        Table table_parent = new Table();
+        ScrollPane pane = new ScrollPane(table);
+        table_parent.add(pane);
+
+        stage.addActor(table_parent);
+        table_parent.setFillParent(true);
+        
 
         Gdx.input.setInputProcessor(stage);
 
---
+++ core/src/com/gdp/thirtysecondsnippets/Results.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.gdp.thirtysecondsnippets;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author George McDaid
+ */
+public class Results {
+    private ArrayList results;
+    
+    public ArrayList getResults() {
+        return results;
+    }
+
+    public void setResults(ArrayList results) {
+        this.results = results;
+    }
+}
