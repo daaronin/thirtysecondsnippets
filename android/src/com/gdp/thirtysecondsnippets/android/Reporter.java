a7cfdb6c18ecb047ed3aac91c305c0ef2d0e641a
thewibblywobbly <georgemcdaid@gmail.com>
2015-04-25 01:09:12 -0400

    Added error reporting

---
+++ android/src/com/gdp/thirtysecondsnippets/android/Reporter.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.gdp.thirtysecondsnippets.android;
+
+import android.app.Application;
+import org.acra.ACRA;
+import org.acra.ReportField;
+import org.acra.ReportingInteractionMode;
+import org.acra.annotation.ReportsCrashes;
+import org.acra.sender.HttpSender;
+
+@ReportsCrashes(
+    formUri = "https://30secondsnippets.cloudant.com/acra-snippets/_design/acra-storage/_update/report",
+    reportType = HttpSender.Type.JSON,
+    httpMethod = HttpSender.Method.POST,
+    formUriBasicAuthLogin = "theretstattlandendartedr",
+    formUriBasicAuthPassword = "B0B6M28knWGEpPDbA2PgUWPh",
+    formKey = "", // This is required for backward compatibility but not used
+    mode = ReportingInteractionMode.TOAST,
+    resToastText = R.string.toast_crash
+    
+)
+
+public class Reporter extends Application{
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // The following line triggers the initialization of ACRA
+        ACRA.init(this);
+    }
+}
